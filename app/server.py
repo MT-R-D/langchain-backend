8c3e4adc93aa14215fe02eb7379fdedf6e2e1933
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2024-01-22 17:54:50 +0200

    start backend-langchain

---
+++ app/server.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+"""Example LangChain server exposes a conversational retrieval agent.
+
+Please see documentation for custom agent streaming here:
+
+https://python.langchain.com/docs/modules/agents/how_to/streaming#stream-tokens
+
+**ATTENTION**
+To support streaming individual tokens you will need to manually set the streaming=True
+on the LLM and use the stream_log endpoint rather than stream endpoint.
+"""
+from typing import Any
+
+from fastapi import FastAPI
+from langchain.agents import AgentExecutor, tool
+from langchain.agents.format_scratchpad import format_to_openai_functions
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.pydantic_v1 import BaseModel
+from langchain.tools.render import format_tool_to_openai_function
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+from langserve import add_routes
+load_dotenv()
+vectorstore = FAISS.from_texts(
+    ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+
+@tool
+def get_eugene_thoughts(query: str) -> list:
+    """Returns Eugene's thoughts on a topic."""
+    return retriever.get_relevant_documents(query)
+
+
+tools = [get_eugene_thoughts]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+# We need to set streaming=True on the LLM to support streaming individual tokens.
+# when using the stream_log endpoint.
+# .stream for agents streams action observation pairs not individual tokens.
+llm = ChatOpenAI(streaming=True)
+
+llm_with_tools = llm.bind(functions=[format_tool_to_openai_function(t) for t in tools])
+
+agent = (
+    {
+        "input": lambda x: x["input"],
+        "agent_scratchpad": lambda x: format_to_openai_functions(
+            x["intermediate_steps"]
+        ),
+    }
+    | prompt
+    | llm_with_tools
+    | OpenAIFunctionsAgentOutputParser()
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="Spin up a simple api server using Langchain's Runnable interfaces",
+)
+
+
+# We need to add these input/output schemas because the current AgentExecutor
+# is lacking in schemas.
+class Input(BaseModel):
+    input: str
+
+
+class Output(BaseModel):
+    output: Any
+
+
+# Adds routes to the app for using the chain under:
+# /invoke
+# /batch
+# /stream
+add_routes(app, agent_executor.with_types(input_type=Input, output_type=Output))
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
